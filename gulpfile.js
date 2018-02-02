24757719bc7fa375bfafc02c3a1b1bc63bd9f5dc
zhang baohua <zhangbh33@gmail.com>
2018-02-02 22:33:58 +0800

    增加html压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp'),
+	htmlMinify = require('gulp-html-minify'),
 	uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
 	gulp.src('*.html')
+	.pipe(htmlMinify())
 	.pipe(gulp.dest('build/'));
 
 	gulp.src('pay-select/*.html')
+	.pipe(htmlMinify())
 	.pipe(gulp.dest('build/pay-select/'));
 
 	gulp.src('pay-select/*.js')
