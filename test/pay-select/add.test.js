0c9e76cad6391261957781fb7f0dfd5d34ee5823
zhang baohua <zhangbh33@gmail.com>
2018-02-28 22:50:42 +0800

    添加测试工具及目录

---
+++ test/pay-select/add.test.js
@@ -0,0 +1,12 @@
+// var add = require('../../pay-select/add'),
+// 	expect = require('chai').expect;
+
+// describe('加法函数的测试', function () { // 测试套件
+// 	it('1加1应该等于2', function () { // 测试用例
+// 		expect(add(1, 1)).to.be.equals(2);
+// 	});
+
+// 	it('2加1应该等于3', function () { // 测试用例
+// 		expect(add(2, 1)).to.be.equals(3);
+// 	});
+// });
